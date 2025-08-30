bb6f426b51f7a9eedecfaaea9695e4f964f4a041
aengus <oleg.durasov.2016@gmail.com>
2025-08-30 23:49:51 +0300

    fix crash during emulator checkup

--- app/src/main/java/com/aengussong/evilappshowcase/analysis_detectors/EmulatorDetector.kt
+++ app/src/main/java/com/aengussong/evilappshowcase/analysis_detectors/EmulatorDetector.kt
@@ -15,46 +15,46 @@ enum class EmulatorProbability {
 object EmulatorDetector {
     fun emulatorCheckup(telephonyManager: TelephonyManager): Map<String, EmulatorProbability> {
         return mapOf(
-            checkProperty("Build.CPU_ABI", Build.CPU_ABI, probablyEmulatorCanary = { it == "armeabi" }),
-            checkProperty("Build.CPU_ABI2", Build.CPU_ABI2, probablyEmulatorCanary = { it == "unknown" }),
-            checkProperty("Build.BOARD", Build.BOARD, emulatorCanary = { it == "unknown" }),
-            checkProperty("Build.BRAND", Build.BRAND, emulatorCanary = { it.beginsWith("generic") }),
-            checkProperty("Build.DEVICE", Build.DEVICE, emulatorCanary = { it.beginsWith("generic") }),
-            checkProperty("Build.FINGERPRINT", Build.FINGERPRINT, emulatorCanary = {
+            checkProperty("Build.CPU_ABI", { Build.CPU_ABI }, probablyEmulatorCanary = { it == "armeabi" }),
+            checkProperty("Build.CPU_ABI2", { Build.CPU_ABI2 }, probablyEmulatorCanary = { it == "unknown" }),
+            checkProperty("Build.BOARD", { Build.BOARD }, emulatorCanary = { it == "unknown" }),
+            checkProperty("Build.BRAND", { Build.BRAND }, emulatorCanary = { it.beginsWith("generic") }),
+            checkProperty("Build.DEVICE", { Build.DEVICE }, emulatorCanary = { it.beginsWith("generic") }),
+            checkProperty("Build.FINGERPRINT", { Build.FINGERPRINT }, emulatorCanary = {
                 it.beginsWith("generic") || it.beginsWith("unknown")
             }),
-            checkProperty("Build.HARDWARE", Build.HARDWARE, emulatorCanary = { it.includes("goldfish") }),
-            checkProperty("Build.HOST", Build.HOST, probablyEmulatorCanary = { it == "android-test" }),
-            checkProperty("Build.ID", Build.ID, emulatorCanary = { it == "FRF91" }),
-            checkProperty("Build.MANUFACTURER", Build.MANUFACTURER, emulatorCanary = {
+            checkProperty("Build.HARDWARE", { Build.HARDWARE }, emulatorCanary = { it.includes("goldfish") }),
+            checkProperty("Build.HOST", { Build.HOST }, probablyEmulatorCanary = { it == "android-test" }),
+            checkProperty("Build.ID", { Build.ID }, emulatorCanary = { it == "FRF91" }),
+            checkProperty("Build.MANUFACTURER", { Build.MANUFACTURER }, emulatorCanary = {
                 it == "unknown" || it.includes("Genymotion")
             }),
-            checkProperty("Build.MODEL", Build.MODEL, emulatorCanary = {
+            checkProperty("Build.MODEL", { Build.MODEL }, emulatorCanary = {
                 it == "sdk" ||
                         it.includes("google_sdk") ||
                         it.includes("Emulator") ||
                         it.includes("Android SDK built for x86")
             }),
-            checkProperty("Build.PRODUCT", Build.PRODUCT, emulatorCanary = {
+            checkProperty("Build.PRODUCT", { Build.PRODUCT }, emulatorCanary = {
                 it == "sdk" ||
                         it == "google_sdk" ||
                         it == "sdk_x86"
             }),
-            checkProperty("Build.RADIO", Build.RADIO, probablyEmulatorCanary = { it == "unknown" }),
+            checkProperty("Build.RADIO", { Build.RADIO }, probablyEmulatorCanary = { it == "unknown" }),
             checkProperty(
                 "Build.SERIAL",
-                Build.SERIAL,
+                { Build.SERIAL },
                 emulatorCanary = { it == null || it == "null" }),
-            checkProperty("Build.USER", Build.USER, emulatorCanary = { it == "android-build" }),
+            checkProperty("Build.USER", { Build.USER }, emulatorCanary = { it == "android-build" }),
 
-            checkProperty("TelephonyManager.getNetworkCountryIso()", telephonyManager.networkCountryIso, probablyEmulatorCanary = { it == "us" }),
-            checkProperty("TelephonyManager.getNetworkOperator().substring(0,3)", telephonyManager.networkOperator?.substring(0, 3), probablyEmulatorCanary = { it == "310" }),
-            checkProperty("TelephonyManager.getNetworkOperator().substring(3)", telephonyManager.networkOperator?.substring(3), probablyEmulatorCanary = { it == "260" }),
-            checkProperty("TelephonyManager.getPhoneType()", telephonyManager.phoneType.toString(), probablyEmulatorCanary = { it == "1" }),
-            checkProperty("TelephonyManager.getSimCountryIso()", telephonyManager.simCountryIso, probablyEmulatorCanary = { it == "us" }),
-            checkProperty("TelephonyManager.getNetworkOperatorName()", telephonyManager.networkOperatorName, probablyEmulatorCanary = { it == "Android" }),
+            checkProperty("TelephonyManager.getNetworkCountryIso()", { telephonyManager.networkCountryIso }, probablyEmulatorCanary = { it == "us" }),
+            checkProperty("TelephonyManager.getNetworkOperator().substring(0,3)", { telephonyManager.networkOperator?.substring(0, 3) }, probablyEmulatorCanary = { it == "310" }),
+            checkProperty("TelephonyManager.getNetworkOperator().substring(3)", { telephonyManager.networkOperator?.substring(3) }, probablyEmulatorCanary = { it == "260" }),
+            checkProperty("TelephonyManager.getPhoneType()", { telephonyManager.phoneType.toString() }, probablyEmulatorCanary = { it == "1" }),
+            checkProperty("TelephonyManager.getSimCountryIso()", { telephonyManager.simCountryIso }, probablyEmulatorCanary = { it == "us" }),
+            checkProperty("TelephonyManager.getNetworkOperatorName()", { telephonyManager.networkOperatorName }, probablyEmulatorCanary = { it == "Android" }),
 
-            checkProperty("ro.kernel.qemu", System.getProperty("ro.kernel.qemu"), emulatorCanary = { it != null}),
+            checkProperty("ro.kernel.qemu", { System.getProperty("ro.kernel.qemu") }, emulatorCanary = { it != null }),
 
             checkFile("/dev/docket/qemud"),
             checkFile("/dev/qemu_pipe"),
@@ -66,19 +66,23 @@ object EmulatorDetector {
 
     private fun checkProperty(
         propertyName: String,
-        propertyValue: String?,
+        propertyValue: () -> String?,
         emulatorCanary: (String?) -> Boolean = { false },
         probablyEmulatorCanary: (String?) -> Boolean = { false }
     ): Pair<String, EmulatorProbability> {
-        fun getProbability(): EmulatorProbability {
-            if (emulatorCanary(propertyValue)) return EmulatorProbability.IS_EMULATOR
-            if (probablyEmulatorCanary(propertyValue)) return EmulatorProbability.PROBABLY_EMULATOR
+        fun getProbability(): ProbabilityResult {
+            val value = propertyValue()
+            var probability = EmulatorProbability.NOT_EMULATOR
 
-            return EmulatorProbability.NOT_EMULATOR
+            if (emulatorCanary(value)) probability = EmulatorProbability.IS_EMULATOR
+            if (probablyEmulatorCanary(value)) probability = EmulatorProbability.PROBABLY_EMULATOR
+
+            return value to probability
         }
 
-        val entry = "$propertyName: $propertyValue"
-        return entry to getProbability()
+        val (value, probability) = safeCheck(::getProbability)
+        val entry = "$propertyName: $value"
+        return entry to probability
     }
 
     private fun checkFile(
@@ -96,4 +100,10 @@ object EmulatorDetector {
         val entry = "$filePath file ${if (fileExists) "was found" else "not found"}"
         return entry to getProbability()
     }
+
+    private fun safeCheck(emulatorCheck: () -> ProbabilityResult): ProbabilityResult {
+        return runCatching { emulatorCheck() }.getOrNull() ?: ("" to EmulatorProbability.NOT_EMULATOR)
+    }
 }
+
+typealias ProbabilityResult = Pair<String?, EmulatorProbability>
